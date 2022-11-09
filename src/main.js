860e12eb3a88fa75fcf43e13e9e73bf331aa2780
Ciara Thyne <ciara.thyne@gmail.com>
2022-11-09 12:45:35 -0800

    added solution

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-const promise = welcome();
+// //original code
+// const promise = welcome();
+// console.log(promise);
 
-console.log(promise);
+
+const welcomePromise = welcome();
+welcomePromise
+    .then((result) => {
+        console.log("The result is:", result);
+    });
+
+
+const goodbyePromise = goodbye()
+    goodbyePromise.then((result) =>{
+    console.log("Here is your fortune:", result);
+    })
+    .catch((error) => {
+    console.log("You have no forturne:",error); 
+    });
+
+const question = "Is this real?"
+const tellPromise = tell(question)
+  tellPromise
+    .then((answer) => {
+        console.log(question)
+        console.log("Success:", answer);
+    })
+    .catch((error) => {
+        console.log("Failure:", error);
+    });
+
+setTimeout(() => console.log(tellPromise), 500);
+
